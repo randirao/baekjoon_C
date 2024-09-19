5d729979dbdf53212a4318789eb6518b05d5dd6f
Han2 <randirao4245@gmail.com>
2024-09-19 23:03:43 +0900

    1032

---
+++ 1032.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    char c[50][50];
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        scanf("%s", c[i]);
+        for(int j=0; j<sizeof(c[i]); j++){
+            if()
+        }
+    }
+    return 0;
+}
