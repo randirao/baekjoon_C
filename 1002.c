21c37a393dea577f3422ab9a91a65bdc1d280cde
Han2 <randirao4245@gmail.com>
2024-09-20 09:48:32 +0900

    1002

--- 1002.c
+++ 1002.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
 int main(){
-    int T;
-    scanf("%d",&T);
-    
+    int t, x1, y1, r1, x2, y2, r2;
+    double d;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++){
+        scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+        d = sqrt((pow(x2-x1, 2)) + (pow(y2-y1, 2)));
+        if(x1 == x2 && y1 == y2 && r1 == r2) printf("-1\n");
+        else if(abs(r1-r2) < d && r1+r2 > d) printf("2\n");
+        else if(abs(r1-r2) == d || r1+r2 == d) printf("1\n");
+        else if(abs(r1-r2) > d || r1+r2 < d) printf("0\n");
+    }
     return 0;
 }
