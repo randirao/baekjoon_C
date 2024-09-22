f5d4e05462d73868235aa7627def294c293b62c9
Han2 <randirao4245@gmail.com>
2024-09-22 22:02:14 +0900

    1003

--- 1003.c
+++ 1003.c
@@ -1,50 +1,23 @@
 #include<stdio.h>
 
-//0과 1출력 횟수
-int cnt[2] = {0};
-//피보나치 자리가 비었는지 확인
-long long memo[41];
+long long memo0[41] = {1, 0};
+long long memo1[41] = {0, 1};
 
-int f(int n){
-
-    if(n==0){
-        cnt[0]++;
-        return 0;
-    }
-
-    else if(n==1){
-        cnt[1]++;
-        return 1;
-    }
-
-    //메모에 값이 들어가 있으면 memo[n] 리턴
-    if(memo[n] != -1) return memo[n];
-
-    //값이 없으면 재귀적으로 계산
-    memo[n] =  f(n-1) + f(n-2);
-    return memo[n];
+void f(int n){
+    if(n==1||n==0) return;
+    if(!memo0[n-1]) f(n-1);
+    memo0[n] = memo0[n-1] + memo0[n-2];
+    memo1[n] = memo1[n-1] + memo1[n-2];
+    return;
 }
 
 int main(){
-
     int t, n;
-
     scanf("%d", &t);
-
     for(int i=0; i<t; i++){
-        //입력
         scanf("%d", &n);
-
-        //초기화
-        for(int i=0; i<41; i++) memo[i] = -1;
-        cnt[0] = 0;
-        cnt[1] = 0;
-
-        //함수 호출
         f(n);
-
-        //출력
-        printf("%d %d\n", cnt[0], cnt[1]);
+        printf("%d %d\n", memo0[n], memo1[n]);
     }
     return 0;
 }
